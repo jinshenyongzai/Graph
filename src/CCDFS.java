ecb3765dd1f58ad11fbbcfeba9ed30a25210c962
jinshenyongzai <1021866202@qq.com>
2019-11-08 21:00:57 +0800

    添加顶点连通性判断功能

--- src/CCDFS.java
+++ src/CCDFS.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CCDFS {
@@ -34,16 +35,44 @@ public class CCDFS {
         return ccount;
     }
 
+    public boolean isConnected(int v, int w){
+        G.validateVertex(v);
+        G.validateVertex(w);
+        return visited[v] == visited[w];
+    }
+
+    public ArrayList<Integer>[] components(){
+
+        ArrayList<Integer>[] res = new ArrayList[ccount];
+        for (int i = 0; i < ccount; i++)
+            res[i] = new ArrayList<>();
+
+        for (int v = 0; v < G.V(); v++)
+            res[visited[v]].add(v);
+        return res;
+    }
+
     public static void main(String[] args) {
 
         // 连通分量为1的图
-        Graph g2 = new Graph("g2.txt");
-        CCDFS ccDFS2 = new CCDFS(g2);
-        System.out.println(ccDFS2.ccount());
+        // Graph g2 = new Graph("g2.txt");
+        // CCDFS ccDFS2 = new CCDFS(g2);
+        // System.out.println(ccDFS2.ccount());
 
         // 连通分量为2的图
         Graph g3 = new Graph("g3.txt");
         CCDFS ccDFS3 = new CCDFS(g3);
         System.out.println(ccDFS3.ccount());
+
+        System.out.println(ccDFS3.isConnected(0,6));
+        System.out.println(ccDFS3.isConnected(0,5));
+
+        ArrayList<Integer>[] comp = ccDFS3.components();
+        for (int ccid = 0; ccid < comp.length; ccid++){
+            System.out.print(ccid + " : ");
+            for (int w: comp[ccid])
+                System.out.print(w + " ");
+            System.out.println();
+        }
     }
 }
