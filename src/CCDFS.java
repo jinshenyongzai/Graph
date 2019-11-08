1ba6cdac8c6d8059cc5f57043479c822d1c2c969
jinshenyongzai <1021866202@qq.com>
2019-11-08 20:49:17 +0800

    求解每个连通分量的顶点

--- src/CCDFS.java
+++ src/CCDFS.java
@@ -1,29 +1,36 @@
+import java.util.Arrays;
+
 public class CCDFS {
 
     private Graph G;
-    private boolean[] visited;
+    private int[] visited;
     private int ccount = 0;
 
     public CCDFS(Graph G) {
 
         this.G = G;
-        visited = new boolean[G.V()];
+        visited = new int[G.V()];
+        Arrays.fill(visited, -1);
+
         for (int v = 0; v < G.V(); v++)
-            if (!visited[v]) {
-                dfs(v);
+            if (visited[v] == -1) {
+                dfs(v, ccount);
                 ccount++;
             }
     }
 
-    private void dfs(int v) {
+    private void dfs(int v, int ccid) {
 
-        visited[v] = true;
+        visited[v] = ccid;
         for (int w : G.adj(v))
-            if (!visited[w])
-                dfs(w);
+            if (visited[w] == -1)
+                dfs(w, ccid);
     }
 
     public int ccount(){
+        for (int e: visited)
+            System.out.print(e + " ");
+        System.out.println();
         return ccount;
     }
 
@@ -32,11 +39,11 @@ public class CCDFS {
         // 连通分量为1的图
         Graph g2 = new Graph("g2.txt");
         CCDFS ccDFS2 = new CCDFS(g2);
-        System.out.println(ccDFS2.ccount);
+        System.out.println(ccDFS2.ccount());
 
         // 连通分量为2的图
         Graph g3 = new Graph("g3.txt");
         CCDFS ccDFS3 = new CCDFS(g3);
-        System.out.println(ccDFS3.ccount);
+        System.out.println(ccDFS3.ccount());
     }
 }
