f56bf9e01d0e7fb43557844ff02687b603f88572
jinshenyongzai <1021866202@qq.com>
2019-11-10 15:34:16 +0800

    实现桥的算法

--- src/FindBridges.java
+++ src/FindBridges.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class FindBridges {
+
+    private Graph G;
+    private boolean[] visited;
+
+    private int ord[];
+    private int low[];
+    private int cnt;
+
+    private ArrayList<Edge> res;
+
+    public FindBridges(Graph G) {
+
+        this.G = G;
+        visited = new boolean[G.V()];
+
+        res = new ArrayList<>();
+        ord = new int[G.V()];
+        low = new int[G.V()];
+        cnt = 0;
+
+        for (int v = 0; v < G.V(); v++)
+            if (!visited[v])
+                dfs(v, v);
+    }
+
+    private void dfs(int v, int parent) {
+
+        visited[v] = true;
+        ord[v] = cnt;
+        low[v] = ord[v];
+        cnt++;
+
+        for (int w : G.adj(v))
+            if (!visited[w]) {
+                dfs(w, v);
+                low[v] = Math.min(low[v], low[w]);
+                if (low[w] > ord[v])
+                    res.add(new Edge(v, w));
+
+            }
+            else if (w != parent)
+                low[v] = Math.min(low[v], low[w]);
+    }
+
+    public ArrayList<Edge> result(){
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        Graph g = new Graph("g6.txt");
+        FindBridges fb = new FindBridges(g);
+        System.out.println(fb.result());
+
+        Graph g2 = new Graph("g7.txt");
+        FindBridges fb2 = new FindBridges(g2);
+        System.out.println(fb2.result());
+
+        Graph tree = new Graph("tree.txt");
+        FindBridges fb3 = new FindBridges(tree);
+        System.out.println(fb3.result());
+    }
 }
