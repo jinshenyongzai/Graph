2869ecb68da2a33e862987669bafb50f608801c2
jinshenyongzai <1021866202@qq.com>
2019-11-27 14:51:47 +0800

    Dijkstra算法的优化

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
 
@@ -6,6 +7,21 @@ public class Dijkstra {
     private int s;
     private int[] dis;
     private boolean[] visited;
+
+    private class Node implements Comparable<Node>{
+
+        public int v, dis;
+
+        public Node(int v, int dis) {
+            this.v = v;
+            this.dis = dis;
+        }
+
+        @Override
+        public int compareTo(Node another) {
+            return dis - another.dis;
+        }
+    }
 
     public Dijkstra(WeightedGraph G, int s){
 
@@ -20,23 +36,40 @@ public class Dijkstra {
 
         visited = new boolean[G.V()];
 
-        while (true){
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        pq.add(new Node(s, 0));
+//        while (true){
+//
+//            int curdis = Integer.MAX_VALUE, cur = -1;
+//            for (int v = 0; v < G.V(); v++)
+//                if (!visited[v] && dis[v] < curdis){
+//                    curdis = dis[v];
+//                    cur = v;
+//                }
+//
+//            if (cur == -1)
+//                break;
+//
+//            visited[cur] = true;
+//            for (int w: G.adj(cur))
+//                if (!visited[w]){
+//                    if (dis[cur] + G.getWeight(cur, w) < dis[w])
+//                        dis[w] = dis[cur] + G.getWeight(cur, w);
+//                }
+//        }
+        while (!pq.isEmpty()) {
 
-            int curdis = Integer.MAX_VALUE, cur = -1;
-            for (int v = 0; v < G.V(); v++)
-                if (!visited[v] && dis[v] < curdis){
-                    curdis = dis[v];
-                    cur = v;
-                }
-
-            if (cur == -1)
-                break;
+            int cur = pq.remove().v;
+            if (visited[cur])
+                continue;
 
             visited[cur] = true;
-            for (int w: G.adj(cur))
-                if (!visited[w]){
-                    if (dis[cur] + G.getWeight(cur, w) < dis[w])
+            for (int w : G.adj(cur))
+                if (!visited[w]) {
+                    if (dis[cur] + G.getWeight(cur, w) < dis[w]) {
                         dis[w] = dis[cur] + G.getWeight(cur, w);
+                        pq.add(new Node(w, dis[w]));
+                    }
                 }
         }
     }
